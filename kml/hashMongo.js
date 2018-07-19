8e18a999b5c272376a5f62bf37dcdb09231878fd
Robert Hopland <robert.hopland@linkmobility.com>
2018-07-19 10:03:10 +0200

    Work

--- kml/hashMongo.js
+++ kml/hashMongo.js
@@ -7,26 +7,54 @@ const mongojs = require('mongojs');
 const { spawn } = require('child_process');
 const argv = require('minimist')(process.argv.slice(2));
 
+var pattern;
 if(!argv._.length) 
+ 	pattern = /.*/;
+else
+	pattern = new RegExp(argv._);
+
+if(!argv.z) 
  	process.exit();
 
-if(!argv.f) 
- 	process.exit();
+ const zoom = argv.z;
 
-if(!argv.t) 
- 	process.exit();
+var db = mongojs('nl3');
+var featuresCollection = db.collection('features'); 
+var hashesCollection = db.collection('hashes'); 
 
 var q = async.queue(function(task, callback) {
-	console.dir(task);
+	console.dir(task._id);
 
-	//const child = spawn('node', ['child_polyfill.js', task, '-f', argv.f, '-t', argv.t]);
+	var km = h3.edgeLength(zoom,'km');
+	var geojson = task.feature;
+	
+	var grid = turf.pointGrid(turf.bbox(geojson),km,{mask:geojson});
 
-	//child.on('exit', function (code, signal) {
-	//	if(code) console.log('child process exited with ' + `code ${code} and signal ${signal}`);
-	  	callback();
-	//});
+	var points = _
+		.chain(grid.features)
+		.map((f) => {
+			var c = f.geometry.coordinates;
+			return h3.geoToH3(c[1], c[0], zoom);
+		})
+		.value();
 
-}, 1);
+	const inside = new Set();
+	for (var i = points.length - 1; i >= 0; i--) {
+		var r = h3.h3ToGeo(points[i],true);
+		var p = turf.point(_.reverse(r));
+		if(turf.inside(p,geojson)) inside.add(points[i])
+	};
+
+
+	var newKey = task._id + '-' + zoom;
+	var newDoc = {
+		_id: newKey,
+		hashes: Array.from(inside)
+	};
+
+	hashesCollection.insert(newDoc, callback);
+
+}, 3);
 
 // assign a callback
 q.drain = function() {
@@ -35,14 +63,13 @@ q.drain = function() {
 };
 
 
-var db = mongojs('nl3');
-var featuresCollection = db.collection('features');
-
-featuresCollection.find().forEach(function(err, feature) {
-    if (!feature) {
+featuresCollection.find({_id : pattern }, function(err, features) {
+    if (!features) {
     	return;
     }
-        
-    q.push(feature._id);
+    _.each(features, (f) => {
+    	q.push(f);
+    });
+
 });
 
