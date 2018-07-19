bb50cd5bae4acf0fe300368f0b7b1362291c4faa
Robert Hopland <robert.hopland@linkmobility.com>
2018-07-19 23:50:44 +0200

    use mongo

--- kml/index.js
+++ kml/index.js
@@ -7,110 +7,46 @@ const clip = require('geojson-clip-polygon');
 const _ = require('lodash');
 var async = require("async");
 const h3 = require('h3-js');
-var Redis = require('ioredis');
 
-var redis = new Redis(7777);
+const mongojs = require('mongojs');
+var db = mongojs('nl3');
+var featuresCollection = db.collection('features'); 
+var hashesCollection = db.collection('hashes'); 
 
 const restify = require('restify');
 const server = restify.createServer();
 
 server.use(restify.plugins.bodyParser({}));
 
-//const luftrom = JSON.parse(fs.readFileSync('luftrom.geojson', 'utf8'));
 
-const luftrom_tpj = JSON.parse(fs.readFileSync('luftrom.topojson', 'utf8'));
+server.get('/api/cell/:zoom/:id', function(req,res,next){
 
-const kommuner_tpj = JSON.parse(fs.readFileSync('kommuner_med_hav.topojson', 'utf8'));
+  var id = req.params.id + '-' + req.params.zoom;
 
-const outline = turf.flatten(topojson.merge(kommuner_tpj,kommuner_tpj.objects.kommuner_med_hav.geometries));
-const luftrom = topojson.feature(luftrom_tpj,luftrom_tpj.objects.luftrom);
+	hashesCollection.findOne({_id : id }, function(err, o) {
+		  	if(err || !o){
+		  		res.send(404);
+		  		return next();
+		  	}
 
-
-
-var clipped = [];
-var collection;
-/*
-async.eachOfSeries(
-	luftrom.features,
-	function(feature,key,callback){
-
-		var new_feature = turf.concave( turf.explode(feature));
-
-		new_feature = intersect.default(new_feature,outline.features[0]);
-
-		if(new_feature){
-			new_feature.properties = feature.properties;
-			clipped.push(new_feature);	
-		} 
-		callback();
-
-	},
-	function(err){
-		console.log(err);
-		collection = turf.featureCollection(clipped);
-	}
-);
-*/
-
-const classC = gjfilter(luftrom, ["==", "class", "C"]);
-
-
-
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
-
-
-server.get('/luftrom',function(req, res, next){
-  res.send(gjfilter(collection, ["==", "class", "G"]));
-  next();
+		  var geo = h3.h3SetToMultiPolygon(o.hashes,true);
+			geo[0] = _.filter(geo[0],function(d){
+				return d.length > 2;
+			});
+		 	res.send(turf.multiPolygon(geo));	
+		 	next();
+	});
 });
 
-server.get('/luftrom_c',function(req, res, next){
-  res.send(classC);
-  next();
-});
+server.get('/api/cell/', function(req,res,next){
 
-server.get('/luftrom_tpj',function(req, res, next){
-  res.send(luftrom_tpj);
-  next();
-});
-
-server.get('/luftrom_tpj2',function(req, res, next){
-  res.send(luftrom_tpj2);
-  next();
-});
-
-
-server.get('/outline',function(req, res, next){
-  res.send(outline);
-  next();
-});
-
-server.get('/outline_tpj',function(req, res, next){
-  res.send(kommuner_tpj);
-  next();
-});
-
-server.get('/h3ToGeoBoundary/:h3Address', function(req,res,next){
-	var geo = h3.h3ToGeoBoundary(req.params.h3Address,true);
- 	res.send(turf.polygon([geo]));
- 	next();
-});
-
-server.get('/cell/:zoom/:id', function(req,res,next){
-	redis.smembers('set:'+ req.params.id + ':h3:zoom-' + req.params.zoom, function (err, result) {
-
-	  	if(err) return next(err);
-
-	  	var geo = h3.h3SetToMultiPolygon(result,true);
-		geo[0] = _.filter(geo[0],function(d){
-			return d.length > 2;
-		});
-	 	res.send(turf.multiPolygon(geo));	
-	 	next();
+	featuresCollection.find({}, function(err, os) {
+		  	if(err || !os){
+		  		res.send(404);
+		  		return next();
+		  	}
+		 	res.send(_.map(os, (o) => {return o.properties;}));	
+		 	next();
 	});
 });
 
@@ -214,8 +150,6 @@ server.post('/h3SetToMultiPolygon', function(req,res,next){
  	next();
 });
 
-server.get('/hello/:name', respond);
-
 server.get('/', restify.plugins.serveStatic({
   directory: './public',
   file: 'index.html'
@@ -223,6 +157,6 @@ server.get('/', restify.plugins.serveStatic({
 
 
 
-server.listen(8080, function() {
+server.listen(8081, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
